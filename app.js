5032cb2daa906f8c72ace47d47e86cf8f4f00c1d
Deepali2 <hdgarg@gmail.com>
2019-08-18 21:43:25 -0700

    makes both the right and the left arrows work

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let x = 0;
 
-document.querySelector(".left-arrow").addEventListener("click", () => {
-  x += 90;
+const rotate = () => {
   const cubes = document.querySelectorAll(".cube");
-  console.log(cubes);
   Array.from(cubes).forEach(
     cube => (cube.style.transform = `rotateY(${x}deg)`)
   );
+};
+
+document.querySelector(".left-arrow").addEventListener("click", () => {
+  x += 90;
+  rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("click", () => {
+  x -= 90;
+  rotate();
 });
